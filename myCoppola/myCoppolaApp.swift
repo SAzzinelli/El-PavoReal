a5e372016d40d8fbbfc16e5d4000801ddfb9f94a
Simone Azzinelli <simone.azzinelli@labafirenze.coim>
2025-08-23 12:48:13 +0200

    Initial Commit

---
+++ myCoppola/myCoppolaApp.swift
@@ -0,0 +1,17 @@
+//
+//  myCoppolaApp.swift
+//  myCoppola
+//
+//  Created by Simone Azzinelli on 23/08/25.
+//
+
+import SwiftUI
+
+@main
+struct myCoppolaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
